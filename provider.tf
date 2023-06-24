fd73f01803a0f9c6a8b9946cc14672a8f5e28d4a
Jerry Man <jerryman@MacBook-Pro.local>
2023-06-24 09:56:20 -0400

    added backend state

--- provider.tf
+++ provider.tf
@@ -3,3 +3,11 @@ provider "aws" {
   # access_key = var.my-access-key
   # secret_key = var.my-secret-key
 }
+
+terraform {
+  backend "s3" {
+    bucket = "bucket-for-gh-terraform-state"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
