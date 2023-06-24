05900e79ec37b6942437973f44f8c6d8b361bfff
Jerry Man <jerryman@MacBook-Pro.local>
2023-06-24 09:17:35 -0400

    added tf files

---
+++ variables.tf
@@ -0,0 +1,17 @@
+# provider variables
+variable "my-region" {
+  description = "this is the region where to provision my resources"
+  type        = string 
+  default = "us-east-1"
+}
+variable "my-access-key" {
+  description = "this is access key"
+  type        = string 
+  default = "AKIAX56PWV25KOOVAY4A"
+}
+
+variable "my-secret-key" {
+  description = "this is secret key"
+  type        = string 
+  default = "Fwpl7vJwO4QHaIjAkBgDbbYN6gAiM9tzBxXRA7na"
+}
